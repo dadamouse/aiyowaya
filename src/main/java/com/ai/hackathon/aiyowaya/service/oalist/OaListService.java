fecf23af8e602e074f18b7812fb27def753663b1
weida <weida.chen@linecorp.com>
2021-10-21 20:20:51 +0800

    fix

--- src/main/java/com/ai/hackathon/aiyowaya/service/oalist/OaListService.java
+++ src/main/java/com/ai/hackathon/aiyowaya/service/oalist/OaListService.java
@@ -39,7 +39,7 @@ public class OaListService {
 
             chatBotDataList.add(
                     ChatBotData.builder()
-                               .variableName("oa" + i)
+                               .variableName("id" + i)
                                .value("https://line.me/R/ti/p/" + entity.getOaId())
                                .build()
             );
